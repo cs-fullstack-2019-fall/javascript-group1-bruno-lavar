58ce86c2ec09a46397b50d7c08835346016483af
insideoutzombie <david@web3devs.com>
2019-08-20 10:43:23 -0500

    secret magic

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 alert("Main side bruh!");
-let person1 = './person1';
+import {main} from './person1';
 
-
+main();
 // Use an array for the English translation and a different array for the Spanish translation.
 // Make sure both arrays are outside of the main function so it can be accessed anywhere in the program.
 // Each word in the array should corrispond with the same word/index of the other array.
 
-class Translate {
-  constuctor(english, spanish){
-    this.english = english;
-    this.spanish = spanish;
-  }
-}
-
-let user_choice;
-
-while(user_choice !== "q") {
-  user_choice = prompt(
-    "Enter a number 1-5 or 'q' to quit"
-  );
-
-  switch (user_choice) {
-    case "q":
-      alert("you quit the program");
-      break;
-    case "1":
-      alert("You hit 1");
-      break;
-  }
-}
+// class Translate {
+//   constuctor(english, spanish){
+//     this.english = english;
+//     this.spanish = spanish;
+//   }
+// }
+//
+// let user_choice;
+//
+// while(user_choice !== "q") {
+//   user_choice = prompt(
+//     "Enter a number 1-5 or 'q' to quit"
+//   );
+//
+//   switch (user_choice) {
+//     case "q":
+//       alert("you quit the program");
+//       break;
+//     case "1":
+//       alert("You hit 1");
+//       break;
+//   }
+// }
--- person1.js
+++ person1.js
@@ -16,47 +16,47 @@ class Translate {
 
 let user_choice;
 
-while(user_choice !== "q") {
-  user_choice = prompt(
-    "Enter a number 1-5 or 'q' to quit"
-  );
+function main() {
+  while(user_choice !== "q") {
+    user_choice = prompt(
+      "Enter a number 1-4 or 'q' to quit"
+    );
 
-  switch (user_choice) {
-    case "q":
-      alert("you quit the program");
-      break;
-    case "1":
-      alert("You hit 1");
-      break;
+    switch (user_choice) {
+      case "q":
+        alert("you quit the program");
+        break;
+      case "1":
+        prompt("Enter english word");
+        break;
+      case "2":
+        alert("You hit 2");
+        break;
+      case "3":
+        alert("You hit 3");
+        break;
+      case "4":
+        alert("You hit 4");
+        break;
+    }
   }
 }
 
-// let user_choice;
-
-// while(user_choice !== "6") {
-//   user_choice = prompt(
-//     "Enter a number 1-5 or 6 to quit"
-//   );
-//   switch (user_choice) {
-//     case "6":
-//       console.log("Later");
-//     case "1":
-//       alert(1);
-//       break;
-//     case "2":
-//       alert(2);
-//       break;
-//     case "3":
-//       alert(3);
-//       break;
-//     case "4":
-//       alert(4);
-//       break;
-//     case "5":
-//       alert(5);
-//       break;
+// function english() {
+//   user_input = prompt("Enter a english word to ")
+//
+//   switch (user_input) {
+//     case:
 //   }
 // }
 
+main();
 
-// main();
+
+function english_to_spanish() {
+
+}
+
+function spanish_to_english() {
+
+}
