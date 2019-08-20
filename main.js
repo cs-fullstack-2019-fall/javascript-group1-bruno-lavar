0cbb0327491d06372d1def3869c6d4a136f0104e
insideoutzombie <david@web3devs.com>
2019-08-20 09:35:49 -0500

    mainjs file was born

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Main side bruh!");
+
+// Use an array for the English translation and a different array for the Spanish translation.
+// Make sure both arrays are outside of the main function so it can be accessed anywhere in the program.
+// Each word in the array should corrispond with the same word/index of the other array.
+
+class Translate() {
+  constuctor(english, spanish){
+    this.english = english;
+    this.spanish = spanish;
+  }
+}
