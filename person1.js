5b1e1dad7d24af4d4f34260a789dcf551d24d99a
insideoutzombie <david@web3devs.com>
2019-08-20 09:27:29 -0500

    change it

---
+++ person1.js
@@ -0,0 +1,4 @@
+alert("Test it bruh");
+
+
+Press 1 to list all English to Spanish translations, press 2 to list all Spanish to English translations, press 3 to enter a new English translation, press 4 to enter a new Spanish translation, and press 5 or 'q' to quit.
