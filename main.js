9d9711863c1ca7b01ef0c8c8e84839ae2e79a556
insideoutzombie <david@web3devs.com>
2019-08-20 09:52:23 -0500

    adds the sauce

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 alert("Main side bruh!");
+let person1 = './person1';
+
 
 // Use an array for the English translation and a different array for the Spanish translation.
 // Make sure both arrays are outside of the main function so it can be accessed anywhere in the program.
 // Each word in the array should corrispond with the same word/index of the other array.
 
-class Translate() {
-  constuctor(english, spanish){
-    this.english = english;
-    this.spanish = spanish;
-  }
-}
+main();
--- person1.js
+++ person1.js
@@ -1,4 +1,62 @@
 alert("Test it bruh");
 
 
-// Press 1 to list all English to Spanish translations, press 2 to list all Spanish to English translations, press 3 to enter a new English translation, press 4 to enter a new Spanish translation, and press 5 or 'q' to quit.
+// Press 1 to list all English to Spanish translations,
+// press 2 to list all Spanish to English translations,
+// press 3 to enter a new English translation,
+// press 4 to enter a new Spanish translation, and press
+// 5 or 'q' to quit.
+
+class Translate {
+  constuctor(english, spanish){
+    this.english = english;
+    this.spanish = spanish;
+  }
+}
+
+let user_choice;
+
+while(user_choice !== "q") {
+  user_choice = prompt(
+    "Enter a number 1-5 or 'q' to quit"
+  );
+
+  switch (user_choice) {
+    case "q":
+      alert("you quit the program");
+      break;
+    case "1":
+      alert("You hit 1");
+      break;
+  }
+}
+
+// let user_choice;
+
+// while(user_choice !== "6") {
+//   user_choice = prompt(
+//     "Enter a number 1-5 or 6 to quit"
+//   );
+//   switch (user_choice) {
+//     case "6":
+//       console.log("Later");
+//     case "1":
+//       alert(1);
+//       break;
+//     case "2":
+//       alert(2);
+//       break;
+//     case "3":
+//       alert(3);
+//       break;
+//     case "4":
+//       alert(4);
+//       break;
+//     case "5":
+//       alert(5);
+//       break;
+//   }
+// }
+
+
+// main();
